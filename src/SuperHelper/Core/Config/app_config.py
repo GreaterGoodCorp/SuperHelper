7e33d01ca7294e3f0e6d2e588d2c2c6c5fa4f554
Nguyen Thai Binh <54888003+MunchDev@users.noreply.github.com>
2021-03-29 11:07:44 +0800

    Add extra error handling

--- src/SuperHelper/Core/Config/app_config.py
+++ src/SuperHelper/Core/Config/app_config.py
@@ -2,6 +2,7 @@
 import logging
 import typing
 import json
+import copy
 
 logger = logging.getLogger("SuperHelper.Core.Config")
 logger.setLevel(logging.DEBUG)
@@ -17,28 +18,30 @@ DefaultApplicationConfig: typing.Dict = {
     },
 }
 
-ApplicationConfig: typing.Dict = dict()
+ApplicationConfig: typing.Dict = copy.deepcopy(DefaultApplicationConfig)
 
 
-def load_app_config(config_path: str) -> int:
+def load_app_config(config_path: str) -> None:
     global ApplicationConfig
-    # Initialise newest update to the config table
-    ApplicationConfig = DefaultApplicationConfig
     try:
         with open(config_path) as fp:
             # De-serialise JSON to Python's dict and update
             ApplicationConfig.update(json.load(fp))
     except OSError:
-        logger.exception("Config loader failed!")
-    return 0
+        logger.exception("Config loader failed due to file being unreadable!")
+        raise
+    except json.JSONDecodeError:
+        logger.exception("Config loader failed due to non-decodable values!")
+        raise
+    return
 
 
-def save_app_config(config_path: str) -> int:
+def save_app_config(config_path: str) -> None:
     global ApplicationConfig
     try:
         with open(config_path, "w") as fp:
-            json.dump(ApplicationConfig, fp)
-        return 0
+            json.dump(ApplicationConfig, fp, default=lambda o: None)
     except OSError:
         logger.exception("Config saver failed!")
-        return -1
+        raise
+        
