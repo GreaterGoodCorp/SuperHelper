a2a62481a54039d43d33b1eb2ddffcdb4e45543f
Nguyen Thai Binh <54888003+MunchDev@users.noreply.github.com>
2021-03-30 11:09:24 +0800

    Export core logger initialiser

--- src/SuperHelper/Core/Utils/__init__.py
+++ src/SuperHelper/Core/Utils/__init__.py
@@ -1,6 +1,8 @@
 from .loader import load_installed_modules
 from .command import load_core_commands
+from .logger import initialise_core_logger
 __all__ = [
     "load_installed_modules",
     "load_core_commands",
+    "initialise_core_logger",
 ]
