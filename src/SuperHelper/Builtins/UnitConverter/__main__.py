b2240d989846b74acc37d3162194a8d322d0e1ff
Nguyen Thai Binh <54888003+MunchDev@users.noreply.github.com>
2021-03-30 14:19:14 +0800

    Add template file

--- src/SuperHelper/Builtins/UnitConverter/__main__.py
+++ src/SuperHelper/Builtins/UnitConverter/__main__.py
@@ -1,1 +1,13 @@
+import logging
 
+from SuperHelper.Core.Config import load_module_config
+
+import click
+
+logger = logging.getLogger("SuperHelper.Builtins.UnitConverter")
+
+
+@click.command("convert")
+def main():
+    config = load_module_config("SuperHelper.Builtins.UnitConverter")
+    pass
