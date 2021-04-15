6bbb8dfd809710be8f7fb2e6ad8b896c4a85aa8d
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-15 18:26:49 +0800

    Add tests for module

--- src/SuperHelper/Tests/test_module_FocusEnabler.py
+++ src/SuperHelper/Tests/test_module_FocusEnabler.py
@@ -2,3 +2,58 @@ import sys
 import pytest
 
 from SuperHelper.Core import main_entry
+
+
+@pytest.fixture(scope="module", autouse=True)
+def add_focus():
+    sys.argv = ["helper", "add", "FocusEnabler"]
+    with pytest.raises(SystemExit, match=r"0"):
+        main_entry()
+
+
+def test_help():
+    sys.argv = ["helper", "focus", "--help"]
+    with pytest.raises(SystemExit, match=r"0"):
+        main_entry()
+
+
+def test_add_single():
+    sys.argv = ["helper", "focus", "add", "google.com"]
+    with pytest.raises(SystemExit, match=r"0"):
+        main_entry()
+
+
+def test_add_multiple():
+    sys.argv = ["helper", "focus", "add", "facebook.com", "youtube.com"]
+    with pytest.raises(SystemExit, match=r"0"):
+        main_entry()
+
+
+def test_add_invalid():
+    sys.argv = ["helper", "focus", "add", "123123"]
+    with pytest.raises(SystemExit, match=r"1"):
+        main_entry()
+
+
+def test_remove_single():
+    sys.argv = ["helper", "focus", "remove", "-c", "google.com"]
+    with pytest.raises(SystemExit, match=r"0"):
+        main_entry()
+
+
+def test_remove_multiple():
+    sys.argv = ["helper", "focus", "remove", "-c", "facebook.com", "youtube.com"]
+    with pytest.raises(SystemExit, match=r"0"):
+        main_entry()
+
+
+def test_remove_invalid():
+    sys.argv = ["helper", "focus", "remove", "-c", "123123"]
+    with pytest.raises(SystemExit, match=r"1"):
+        main_entry()
+
+
+def test_list():
+    sys.argv = ["helper", "focus", "list"]
+    with pytest.raises(SystemExit, match=r"0"):
+        main_entry()
