cbeacd4786b73ca837db96e031418399d1d8236d
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-15 18:39:52 +0800

    Re-order import to avoid import conflict

--- src/SuperHelper/Core/Utils/__init__.py
+++ src/SuperHelper/Core/Utils/__init__.py
@@ -1,10 +1,10 @@
+from .type_hinting import *
 from .command import load_core_commands
 from .loader import load_installed_modules
 from .logger import initialise_core_logger
 from .bit_ops import BitOps
 from .crypto_ops import Cryptographer
 from .type_ensure import TypeCheck
-from .type_hinting import *
 
 __all__ = [
     "load_installed_modules",
