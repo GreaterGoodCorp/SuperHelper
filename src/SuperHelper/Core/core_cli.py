0f70985a1fdd8aea12c8f603390e8178452fca45
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-16 13:50:22 +0800

    Refactor main entry function

--- src/SuperHelper/Core/core_cli.py
+++ src/SuperHelper/Core/core_cli.py
@@ -1,5 +1,6 @@
 # This module defines the entry Click CLI function
 # All modules of SuperHelper should define its own group, which can imported by this module.
+import logging
 import os
 import pathlib
 import platform
@@ -22,11 +23,7 @@ LOGGING_FILENAME = f"{APP_NAME}.log"
 LOGGING_PATH = APP_DIR / LOGGING_FILENAME
 
 version_message = f"%(prog)s-%(version)s {platform.platform(terse=True)} Python-{platform.python_version()}"
-
-
-def validate_no_win32() -> None:
-    """This function asserts that the platform is not 'win32'."""
-    assert sys.platform != "win32"
+logger: logging.Logger
 
 
 # Program entry point
@@ -36,31 +33,43 @@ def cli() -> int:
     pass
 
 
-# Console entry call
-def main_entry() -> NoReturn:
-    # Load core logger
-    logger = initialise_core_logger(LOGGING_PATH)
+def validate_no_win32() -> None:
+    """This function asserts that the platform is not 'win32'."""
     try:
-        validate_no_win32()
+        assert sys.platform != "win32"
     except AssertionError:
         logger.exception("This application cannot run on Windows!")
         sys.exit(1)
-    try:
-        # Load application config
-        from SuperHelper.Core.Config import load_app_config, save_app_config
-        load_app_config(CONFIG_PATH)
-    except RuntimeError:
-        sys.exit(1)
-    # Load core utilities and functionalities
+
+
+def make_logger_global():
+    global logger
+    logger = initialise_core_logger(LOGGING_PATH)
+
+
+def load_config():
+    from SuperHelper.Core.Config import load_app_config
+    load_app_config(CONFIG_PATH)
+
+
+def save_config():
+    from SuperHelper.Core.Config import save_app_config
+    save_app_config(CONFIG_PATH)
+
+
+def load_core_commands():
     from SuperHelper.Core.Utils import load_core_commands
     for core_module in load_core_commands():
         try:
             cli.add_command(core_module[0])
         except Exception or BaseException:
             logger.exception(f"Unable to load core module {core_module[1]}")
+            sys.exit(1)
         else:
             logger.debug(f"Loaded core module {core_module[1]}")
-    # Load installed modules
+
+
+def load_added_modules():
     from SuperHelper.Core.Utils import load_installed_modules
     for module in load_installed_modules():
         try:
@@ -69,6 +78,25 @@ def main_entry() -> NoReturn:
             logger.exception(f"Unable to load module {module[1]}")
         else:
             logger.debug(f"Loaded module {module[1]}")
+
+
+def run_startup():
+    # Load core logger
+    make_logger_global()
+    # Validate platform
+    validate_no_win32()
+    # Load config
+    load_config()
+    # Load core utilities and functionalities
+    load_core_commands()
+    # Load installed modules
+    load_added_modules()
+
+
+# Console entry call
+def main_entry() -> NoReturn:
+    # Run startup setting
+    run_startup()
     try:
         # Print all messages before executing CLI
         sys.stdout.flush()
@@ -77,7 +105,7 @@ def main_entry() -> NoReturn:
         sys.exit(cli())
     except SystemExit:
         # Save application config
-        save_app_config(CONFIG_PATH)
+        save_config()
         raise
 
 
