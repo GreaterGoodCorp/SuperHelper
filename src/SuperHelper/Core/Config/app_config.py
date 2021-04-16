5dde0618f348b983d8812513aac1bc1516b60ab6
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-16 13:50:50 +0800

    Exit instead of re-raise

--- src/SuperHelper/Core/Config/app_config.py
+++ src/SuperHelper/Core/Config/app_config.py
@@ -1,6 +1,7 @@
 # This module defines two config functions, load_app_config() and save_app_config()
 import json
 import logging
+import sys
 
 from SuperHelper.Core.Utils import PathLike
 from SuperHelper.Core.Config import Config, DefaultCoreConfig, make_config_global, pass_config
@@ -20,7 +21,7 @@ def load_app_config(config_path: PathLike) -> None:
         make_config_global(Config())
     except OSError:
         logger.exception("Config loader failed due to file being unreadable!")
-        raise RuntimeError
+        sys.exit(1)
 
 
 @pass_config()
@@ -30,4 +31,4 @@ def save_app_config(config: Config, config_path: PathLike) -> None:
             json.dump(config.__dict__(), fp)
     except OSError:
         logger.exception("Config saver failed!")
-        raise RuntimeError
+        sys.exit(1)
