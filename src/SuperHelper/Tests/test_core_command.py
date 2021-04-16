22056f6424d028c4fb8aea80675d601a3b5c7921
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-16 16:42:03 +0800

    Refactor into test class

--- src/SuperHelper/Tests/test_core_command.py
+++ src/SuperHelper/Tests/test_core_command.py
@@ -1,29 +1,31 @@
 from SuperHelper.Tests import *
 
 
-def test_add_single():
-    assert run("add FocusEnabler").exit_code == 0
+class TestCore:
+    @staticmethod
+    def test_add_single():
+        assert run("add FocusEnabler").exit_code == 0
 
+    @staticmethod
+    def test_remove_single():
+        assert run("remove FocusEnabler").exit_code == 0
 
-def test_remove_single():
-    assert run("remove FocusEnabler").exit_code == 0
+    @staticmethod
+    def test_add_multiple():
+        assert run("add FocusEnabler Stenographer").exit_code == 0
 
+    @staticmethod
+    def test_remove_multiple():
+        assert run("remove FocusEnabler Stenographer").exit_code == 0
 
-def test_add_multiple():
-    assert run("add FocusEnabler Stenographer").exit_code == 0
+    @staticmethod
+    def test_add_invalid():
+        assert run("add test").exit_code == 1
 
+    @staticmethod
+    def test_remove_invalid():
+        assert run("remove test").exit_code == 1
 
-def test_remove_multiple():
-    assert run("remove FocusEnabler Stenographer").exit_code == 0
-
-
-def test_add_invalid():
-    assert run("add test").exit_code == 1
-
-
-def test_remove_invalid():
-    assert run("remove test").exit_code == 1
-
-
-def test_list():
-    assert run("list").exit_code == 0
+    @staticmethod
+    def test_list():
+        assert run("list").exit_code == 0
