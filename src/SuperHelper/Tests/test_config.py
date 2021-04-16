0e51a297bb5c3cc3a7bfacfe55802475ef5740a0
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-16 22:53:16 +0800

    Add tests to cover edge cases `pass_config` and `from_dict`

---
+++ src/SuperHelper/Tests/test_config.py
@@ -0,0 +1,35 @@
+import pytest
+
+from SuperHelper.Core import run_startup
+from SuperHelper.Core.Config import Config, pass_config
+
+
+class TestConfig:
+    @staticmethod
+    def test_from_dict():
+        assert Config.from_dict(dict()) is None
+
+    @staticmethod
+    def test_core_and_no_module():
+        @pass_config(core=True, lock=True)
+        def test_a(config):
+            return config
+        run_startup()
+        test_a()
+
+    @staticmethod
+    def test_no_core_and_module():
+        @pass_config(module_name="test", lock=True)
+        def test_a(config):
+            return config
+        run_startup()
+        test_a()
+
+    @staticmethod
+    def test_core_and_module():
+        with pytest.raises(ValueError):
+            @pass_config(core=True, module_name="test", lock=True)
+            def test_a(config):
+                return config
+            run_startup()
+            test_a()
