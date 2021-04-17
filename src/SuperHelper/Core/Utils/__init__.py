06680f31cd9531fdd33858d58e56103cd4ea0e5c
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-17 11:02:56 +0800

    Re-organise imports and add comments

--- src/SuperHelper/Core/Utils/__init__.py
+++ src/SuperHelper/Core/Utils/__init__.py
@@ -1,18 +1,27 @@
-from .type_hinting import *
+# Type hinting utilities
+from .type_hinting import Generic, PathLike
+# Type checking utilities
+from .type_ensure import TypeCheck
+# Logger setup utility
+from .logger import setup_core_logger
+# Core command loader
 from .command import load_core_commands
-from .loader import load_installed_modules
-from .logger import initialise_core_logger
+# Modules loader
+from .loader import load_added_modules
+# Miscellaneous utilities
 from .bit_ops import BitOps
 from .crypto_ops import Cryptographer
-from .type_ensure import TypeCheck
+from .file_ops import FileOps, FP
 
 __all__ = [
-    "load_installed_modules",
-    "load_core_commands",
-    "initialise_core_logger",
-    "BitOps",
-    "Cryptographer",
-    "TypeCheck",
     "PathLike",
     "Generic",
+    "TypeCheck",
+    "setup_core_logger",
+    "load_core_commands",
+    "load_added_modules",
+    "BitOps",
+    "Cryptographer",
+    "FP",
+    "FileOps"
 ]
