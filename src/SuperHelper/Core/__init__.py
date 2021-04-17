8f430f645bc236f1eb961b7e86d9914efb82e962
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-17 17:24:07 +0800

    Re-order import and add comments

--- src/SuperHelper/Core/__init__.py
+++ src/SuperHelper/Core/__init__.py
@@ -1,6 +1,9 @@
-from .core_cli import *
+# Load the frequently used functions from subpackages
+from SuperHelper.Core.Config import pass_config
+from .core_cli import main_entry, run_startup, cli, save_config
 
 __all__ = [
+    "pass_config",
     "main_entry",
     "run_startup",
     "cli",
