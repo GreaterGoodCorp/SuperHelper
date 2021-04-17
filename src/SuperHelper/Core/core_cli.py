bb16936c8a739e45d70a0d24df17a3822922365b
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-17 17:24:30 +0800

    Add docstrings

--- src/SuperHelper/Core/core_cli.py
+++ src/SuperHelper/Core/core_cli.py
@@ -23,18 +23,27 @@ LOGGING_FILENAME = f"{APP_NAME}.log"
 LOGGING_PATH = APP_DIR / LOGGING_FILENAME
 
 version_message = f"%(prog)s-%(version)s {platform.platform(terse=True)} Python-{platform.python_version()}"
-logger: logging.Logger
+logger = logging.getLogger("SuperHelper")
+
+__all__ = [
+    "run_startup",
+    "load_config",
+    "save_config",
+    "main_entry",
+    "cli",
+]
 
 
 # Program entry point
 @click.group()
 @click.version_option(Version, prog_name=APP_NAME, message=version_message)
 def cli() -> None:
+    """Executes SuperHelper tools."""
     pass
 
 
 def validate_no_win32() -> None:
-    """This function asserts that the platform is not 'win32'."""
+    """Asserts that the platform is not 'win32'."""
     try:
         assert sys.platform != "win32"
     except AssertionError:
@@ -42,24 +51,27 @@ def validate_no_win32() -> None:
         sys.exit(1)
 
 
-def make_logger_global():
+def make_logger_global() -> None:
+    """Sets up and makes core logger global."""
     global logger
     logger = setup_core_logger(LOGGING_PATH)
 
 
 def load_config():
+    """Loads application config."""
     from SuperHelper.Core.Config import load_app_config
     load_app_config(CONFIG_PATH)
 
 
 def save_config():
+    """Saves application config."""
     from SuperHelper.Core.Config import save_app_config
     save_app_config(CONFIG_PATH)
 
 
 def load_core_commands():
-    from SuperHelper.Core.Utils import load_core_commands
-    for core_module in load_core_commands():
+    from SuperHelper.Core.Utils import load_core_commands as _load_core_commands
+    for core_module in _load_core_commands():
         try:
             cli.add_command(core_module[0])
         except Exception or BaseException:
@@ -70,8 +82,8 @@ def load_core_commands():
 
 
 def load_added_modules():
-    from SuperHelper.Core.Utils import load_added_modules
-    for module in load_added_modules():
+    from SuperHelper.Core.Utils import load_added_modules as _load_added_modules
+    for module in _load_added_modules():
         try:
             cli.add_command(module[0])
         except Exception or BaseException:
@@ -91,6 +103,9 @@ def run_startup():
     load_core_commands()
     # Load installed modules
     load_added_modules()
+    # Print all messages before executing CLI
+    sys.stdout.flush()
+    sys.stderr.flush()
 
 
 # Console entry call
@@ -98,9 +113,6 @@ def main_entry() -> NoReturn:
     # Run startup setting
     run_startup()
     try:
-        # Print all messages before executing CLI
-        sys.stdout.flush()
-        sys.stderr.flush()
         # Execute CLI
         sys.exit(cli())
     except SystemExit:
@@ -108,14 +120,6 @@ def main_entry() -> NoReturn:
         save_config()
         raise
 
-
-__all__ = [
-    "run_startup",
-    "load_config",
-    "save_config",
-    "main_entry",
-    "cli",
-]
 
 if __name__ == '__main__':
     sys.argv[0] = "SuperHelper"
