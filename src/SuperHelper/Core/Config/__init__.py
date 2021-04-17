a6df5cb87fa4f0107f2bb98c55030a3ad325d1c2
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-17 11:09:04 +0800

    Re-organise imports and add comments

--- src/SuperHelper/Core/Config/__init__.py
+++ src/SuperHelper/Core/Config/__init__.py
@@ -1,11 +1,13 @@
+# Configuration manager implementation
 from .config_class import Config, DefaultCoreConfig, make_config_global, pass_config
+# Configuration loader and saver
 from .app_config import load_app_config, save_app_config
 
 __all__ = [
     "Config",
+    "DefaultCoreConfig",
     "make_config_global",
     "pass_config",
     "load_app_config",
     "save_app_config",
-    "DefaultCoreConfig",
 ]
