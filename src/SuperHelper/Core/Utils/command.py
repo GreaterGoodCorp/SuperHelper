97a7d53ac624867cd49b72a6cd512128ea9951e3
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-17 11:58:32 +0800

    Add docstring and comments to utilities

--- src/SuperHelper/Core/Utils/command.py
+++ src/SuperHelper/Core/Utils/command.py
@@ -1,4 +1,4 @@
-# This module defines core utilities and functionalities
+# This module defines Core CLI utilities and functionalities
 import copy
 import importlib.util
 import logging
@@ -19,6 +19,11 @@ __all__ = [
 
 
 def load_core_commands() -> list[tuple[Callable, str]]:
+    """Returns the Core CLI commands.
+
+    :return: A list of command-name pairs to be added to Core CLI
+    :rtype: list[tuple[Callable, str]]
+    """
     return [
         (add_modules, "core_add"),
         (remove_modules, "core_remove"),
@@ -32,21 +37,29 @@ def load_core_commands() -> list[tuple[Callable, str]]:
 def add_modules(config: dict[str, ...], modules: list[str]) -> None:
     """Adds new modules into SuperHelper."""
     module_prefix = "SuperHelper.Modules.{}"
+    # Deep-copy the list to avoid modifying it in place
     all_modules = copy.deepcopy(config["INSTALLED_MODULES"])
     for module in modules:
+        # Append the module prefix to module name
         module_fullname = module_prefix.format(module)
         try:
+            # Check if the module is found
+            # If the module is installed, it should be located as follows:
+            # SuperHelper.Modules.<ModuleName>
+            # Otherwise, it is not found (or not installed)
             if importlib.util.find_spec(module_fullname) is not None:
                 if module_fullname not in config["INSTALLED_MODULES"]:
                     all_modules.append(module_fullname)
             else:
                 raise ModuleNotFoundError
         except ModuleNotFoundError:
-            logger.exception(f"Module {module} not found!")
+            logger.exception(f"Module {module} not found!\nReverting...")
             break
     else:
+        # Save the config if and only if there are no errors and exit with 0.
         config["INSTALLED_MODULES"] = all_modules
         sys.exit(0)
+    # Otherwise, if there are errors, exit with a non-zero code, i.e. 1.
     sys.exit(1)
 
 
@@ -56,19 +69,26 @@ def add_modules(config: dict[str, ...], modules: list[str]) -> None:
 def remove_modules(config: dict[str, ...], modules: list[str]) -> None:
     """Removes existing modules from SuperHelper."""
     module_prefix = "SuperHelper.Modules.{}"
+    # Deep-copy the list to avoid modifying it in place
     all_modules = copy.deepcopy(config["INSTALLED_MODULES"])
     for module in modules:
+        # Append the module prefix to module name
         module_fullname = module_prefix.format(module)
+        # Check if the module is already added, if so, then remove
         if module_fullname in all_modules:
             all_modules.remove(module_fullname)
+        # Check if there are repeated modules, if so, ignore the repeated one
         elif module_fullname in config["INSTALLED_MODULES"]:
             continue
+        # If the module is not found, i.e. not added, cancel the operations
         else:
-            logger.warning(f"Module {module} not found!")
+            logger.warning(f"Module {module} not found!\nReverting...")
             break
     else:
+        # Save the config if and only if there are no errors and exit with 0
         config["INSTALLED_MODULES"] = all_modules
         sys.exit(0)
+    # Otherwise, if there are errors, exit with a non-zero code, i.e. 1
     sys.exit(1)
 
 
@@ -80,12 +100,18 @@ def list_modules(config: dict[str, ...], list_all: bool) -> None:
     import SuperHelper.Modules as Package
     prefix = Package.__name__ + "."
     count = 0
+    # Iterate over all installed modules found in SuperHelper.Modules
     for _, module_name, _ in pkgutil.iter_modules(Package.__path__, prefix):
+        # If the module is added, print it as installed
+        short_module_name = module_name.split(".")[-1]
         if module_name in config["INSTALLED_MODULES"]:
-            click.echo(f"(Installed) {module_name}")
+            click.echo(f"(Installed) {short_module_name}")
             count += 1
         elif list_all:
-            click.echo(module_name)
+            # Otherwise, if --all is specified, print it as not installed
+            click.echo(short_module_name)
+    # If --all is not specified and there are no installed modules, also report to the user
     if count == 0 and not list_all:
         click.echo("No installed modules found!")
+    # Then exit with 0
     sys.exit(0)
--- src/SuperHelper/Core/Utils/file_ops.py
+++ src/SuperHelper/Core/Utils/file_ops.py
@@ -1,3 +1,5 @@
+# This module defines the FP enum, to specify file permission flags, and the FileOps class, a utility class to check
+# file ownership and permissions.
 import functools
 import os
 import stat
@@ -13,6 +15,21 @@ __all__ = [
 
 
 class FP(enum.Flag):
+    """
+    Contains file permission flags.
+
+    R = Read
+
+    W = Write
+
+    X = Execute
+
+    USR = User (file owner)
+
+    GRP = Group owner
+
+    OTH = Other users/groups
+    """
     R_USR = enum.auto()
     W_USR = enum.auto()
     X_USR = enum.auto()
@@ -25,61 +42,168 @@ class FP(enum.Flag):
 
 
 class FileOps:
+    """
+    A utility class for file ownership and permissions.
+    """
     @staticmethod
     @functools.cache
     def get_stat(path: PathLike) -> os.stat_result:
+        """Gets the `stat` of file pointed by the path.
+
+        This function is decorated by @cache to reduce the amount of syscall, since `os.stat` is an expensive function.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: An os.stat_result instance containing the stat of the file
+        :rtype: os.stat_result
+        """
         return os.stat(path)
 
     @staticmethod
-    def is_user_own(uid: int, path: PathLike):
+    def is_user_own(uid: int, path: PathLike) -> bool:
+        """Checks if the file is owned by the user with `uid`.
+
+        :param uid: The UID of the user
+        :type uid: int
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is owned by the uid, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_uid == uid
 
     @staticmethod
-    def is_roots(path: PathLike):
+    def is_roots(path: PathLike) -> bool:
+        """Check if the file is owned by `root`.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is owned by root, otherwise False
+        :rtype: bool
+        """
         return FileOps.is_user_own(0, path)
 
     @staticmethod
-    def is_mine(path: PathLike):
+    def is_mine(path: PathLike) -> bool:
+        """Checks if the file is owned by the current user.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is owned by the current user, otherwise False
+        :rtype: bool
+        """
         return FileOps.is_user_own(os.getuid(), path)
 
     @staticmethod
-    def is_owner_readable(path: PathLike):
+    def is_owner_readable(path: PathLike) -> bool:
+        """Checks if the owner of the file can read it.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is readable by its owner, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IRUSR
 
     @staticmethod
-    def is_owner_writable(path: PathLike):
+    def is_owner_writable(path: PathLike) -> bool:
+        """Checks if the owner of the file can write to it.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is writable by its owner, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IWUSR
 
     @staticmethod
-    def is_owner_executable(path: PathLike):
+    def is_owner_executable(path: PathLike) -> bool:
+        """Checks if the owner of the file can execute it.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is executable by its owner, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IXUSR
 
     @staticmethod
-    def is_group_readable(path: PathLike):
+    def is_group_readable(path: PathLike) -> bool:
+        """Checks if the group owner of the file can read it.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is readable by its group owner, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IRGRP
 
     @staticmethod
-    def is_group_writable(path: PathLike):
+    def is_group_writable(path: PathLike) -> bool:
+        """Checks if the group owner of the file can write to it.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is writable by its group owner, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IWGRP
 
     @staticmethod
-    def is_group_executable(path: PathLike):
+    def is_group_executable(path: PathLike) -> bool:
+        """Checks if the group owner of the file can execute it.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is executable by its group owner, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IXGRP
 
     @staticmethod
-    def is_other_readable(path: PathLike):
+    def is_other_readable(path: PathLike) -> bool:
+        """Checks if the other users or groups can read the file.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is readable by them, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IROTH
 
     @staticmethod
-    def is_other_writable(path: PathLike):
+    def is_other_writable(path: PathLike) -> bool:
+        """Checks if the other users or groups can write the file.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is writable by them, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IWOTH
 
     @staticmethod
-    def is_other_executable(path: PathLike):
+    def is_other_executable(path: PathLike) -> bool:
+        """Checks if the other users or groups can execute the file.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :return: True if the file is executable by them, otherwise False
+        :rtype: bool
+        """
         return FileOps.get_stat(path).st_mode & stat.S_IXOTH
 
     @staticmethod
-    def check_fp(path: PathLike, fp: FP):
+    def check_fp(path: PathLike, fp: FP) -> bool:
+        """Checks if the file contains the specified file permissions.
+
+        :param path: Path to the file to check
+        :type path: PathLike
+        :param fp: The flags of the file permissions to check.
+        :type fp: FP
+        :return: True if all the flags are valid, otherwise False
+        :rtype: bool
+        """
         flag_match: dict[FP, Callable[[PathLike], bool]] = {
             FP.R_USR: FileOps.is_owner_readable,
             FP.W_USR: FileOps.is_owner_writable,
