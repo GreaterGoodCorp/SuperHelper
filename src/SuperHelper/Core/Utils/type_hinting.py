a63ad9733349878dccd092f736a32bad08386b3a
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-17 22:40:39 +0800

    Remove generic type

--- src/SuperHelper/Core/Utils/type_hinting.py
+++ src/SuperHelper/Core/Utils/type_hinting.py
@@ -3,12 +3,8 @@ import os
 from typing import TypeVar
 
 __all__ = [
-    "Generic",
     "PathLike",
 ]
-
-# Generic type, i.e. any type
-Generic: type = TypeVar("Generic")
 
 # PathLike type, which can be passed as argument to I/O function
 PathLike: type = TypeVar("PathLike", str, bytes, os.PathLike)
