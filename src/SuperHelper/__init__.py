3ef478c78c0c40cbcb5b55b2d5d0f980c9795751
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-18 12:38:18 +0800

    Refactor AppName and AppDir to top-level package

--- src/SuperHelper/__init__.py
+++ src/SuperHelper/__init__.py
@@ -1,5 +1,19 @@
+import pathlib
+import os
+
+import click
+
 __version__ = "1.0.0"
 Version = __version__
+
+AppName = "SuperHelper"
+"""Name of the application."""
+AppDir = pathlib.Path(os.getenv("SUPER_HELPER_APP_DIR", click.get_app_dir(AppName)))
+"""Path to the application directory."""
+AppDir.mkdir(parents=True, exist_ok=True)
+
 __all__ = [
     "Version",
+    "AppName",
+    "AppDir",
 ]
