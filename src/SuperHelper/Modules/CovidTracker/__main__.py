bd195cab3d9b35589a4387632b7ce943d0fbca05
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-04-24 20:15:41 +0800

    Add full docstrings for all exposed functions

--- src/SuperHelper/Modules/CovidTracker/__main__.py
+++ src/SuperHelper/Modules/CovidTracker/__main__.py
@@ -1,5 +1,5 @@
 import csv
-import datetime
+from datetime import datetime, timedelta
 import io
 import json
 import sys
@@ -25,7 +25,7 @@ __name__ = f"SuperHelper.Modules.{MODULE_NAME}"
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
-origin_date = datetime.datetime(day=2, month=12, year=2020)
+origin_date = datetime(day=2, month=12, year=2020)
 
 __all__ = [
     "normalise_datetime",
@@ -42,6 +42,16 @@ __all__ = [
 
 
 def normalise_datetime(date_string: str) -> str:
+    """Turns any date string into format MM-DD-YYYY.
+
+    This function uses `dateutil.parser.parse` internally. Hence, most strings of digits are valid.
+
+    Args:
+        date_string (str): A date string.
+
+    Returns:
+        A date string in format MM-DD-YYYY.
+    """
     try:
         return parse(date_string, dayfirst=True).strftime("%m-%d-%Y")
     except ValueError:
@@ -49,6 +59,14 @@ def normalise_datetime(date_string: str) -> str:
 
 
 def create_source_url(date_string: str) -> str:
+    """Creates the URL to source file of date.
+
+    Args:
+        date_string (str): A date string.
+
+    Returns:
+        A URL string of the source file.
+    """
     if not re.match(r"^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-20\d\d$", date_string):
         raise ValueError("Invalid date format, must be MM-DD-YYY! Try using normalise_datetime()!")
     gh_branch_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master"
@@ -57,7 +75,19 @@ def create_source_url(date_string: str) -> str:
     return gh_branch_url + source_path + file_name
 
 
-def get_source_file(url: str, force: bool = False) -> list:
+def get_source_file(url: str, force: bool = False) -> list[str]:
+    """Downloads and saves source file.
+
+    This function will look for cached source file, if any, then return. If `force` is set to `True`, this function
+    will re-download and overwrite the cached file.
+
+    Args:
+        url (str): The URL to source file.
+        force (bool): Whether to force re-download.
+
+    Returns:
+        A list of strings of the lines of the source file.
+    """
     filename = Path(url).name
     source_file_location = MODULE_DIR / "Cache" / filename
     if not force and source_file_location.is_file():
@@ -70,17 +100,25 @@ def get_source_file(url: str, force: bool = False) -> list:
     return string_data
 
 
-latest_date = datetime.datetime.today()
+latest_date = datetime.today()
 while True:
     try:
         get_source_file(create_source_url(latest_date.strftime("%m-%d-%Y")))
     except HTTPError:
-        latest_date -= datetime.timedelta(days=1)
+        latest_date -= timedelta(days=1)
     else:
         break
 
 
-def parse_source_data(source_data: list[str]) -> list:
+def parse_source_data(source_data: list[str]) -> list[str]:
+    """Parses source data as CSV.
+
+    Args:
+        source_data (list[str]): A list of lines of source data.
+
+    Returns:
+        A list of CSV entries of the source file.
+    """
     concat = "".join(source_data)
     buffer = io.StringIO(concat)
     csv_parsed_data = csv.reader(buffer)
@@ -88,6 +126,17 @@ def parse_source_data(source_data: list[str]) -> list:
 
 
 def extract_source_data(parsed_data: list[str], cache_file: PathLike = None, force: bool = False) -> dict[str, list]:
+    """Extracts information from the parsed source data.
+
+    Args:
+        parsed_data (list[str]): The parsed source data.
+        cache_file (PathLike): Path to cached file.
+        force (bool): Whether to force re-extraction.
+
+    Returns:
+        A dictionary of country-data pairs, where country is the name of a country, and data is a list of confirmed,
+        death, recovered, and active COVID-19 cases of the country, respectively.
+    """
     # Remove header
     parsed_data.pop(0)
     starting_index = 7
@@ -110,7 +159,17 @@ def extract_source_data(parsed_data: list[str], cache_file: PathLike = None, for
     return data
 
 
-def get_data_for_date(date_string: str, force: bool = False):
+def get_data_for_date(date_string: str, force: bool = False) -> dict[str, list]:
+    """Gets the extracted data for date.
+
+    Args:
+        date_string (str): A date string.
+        force (bool): Whether to force re-download and re-extraction.
+
+    Returns:
+        A dictionary of country-data pairs, where country is the name of a country, and data is a list of confirmed,
+        death, recovered, and active COVID-19 cases of the country, respectively.
+    """
     if not re.match(r"^(0[1-9]|1[012])-(0[1-9]|[12][0-9]|3[01])-20\d\d$", date_string):
         raise ValueError("Invalid date format, must be MM-DD-YYY! Try using normalise_datetime()!")
     url = create_source_url(date_string)
@@ -120,14 +179,25 @@ def get_data_for_date(date_string: str, force: bool = False):
     return extract_source_data(parsed_source_file, cache_filename, force)
 
 
-def get_country_data(country: str, start_date: datetime.datetime = None, end_date: datetime.datetime = None):
+def get_country_data(country: str, start_date: datetime = None, end_date: datetime = None) -> dict[str, list]:
+    """Gets data for a country over a range of days.
+
+    Args:
+        country (str): The name of the country.
+        start_date (datetime): The starting date.
+        end_date (datetime): The ending date.
+
+    Returns:
+        A dictionary of date-data pairs, where date is the date when the data is obtained, and data is a list of
+        confirmed, death, recovered, and active COVID-19 cases of the country, respectively.
+    """
     if end_date is None or end_date > latest_date:
         end_date = latest_date
     if start_date is None or start_date < origin_date:
         start_date = origin_date
     result = dict()
     for i in range((end_date - start_date).days + 1):
-        date_string = (start_date + datetime.timedelta(days=i)).strftime("%m-%d-%Y")
+        date_string = (start_date + timedelta(days=i)).strftime("%m-%d-%Y")
         data = get_data_for_date(date_string)
         country_data = data.get(country, None)
         if country_data is not None:
@@ -138,6 +208,17 @@ def get_country_data(country: str, start_date: datetime.datetime = None, end_dat
 
 
 def cache_data(no_of_days: int = 365, force: bool = False) -> None:
+    """Downloads, extracts and cache data.
+
+    This function is a helper function.
+
+    Args:
+        no_of_days (int): Number of days worth of data to download.
+        force (bool): Whether to force re-download and re-extraction.
+
+    Returns:
+        None
+    """
     date = deepcopy(latest_date)
     date_string = date.strftime("%m-%d-%Y")
     if (date - origin_date).days < no_of_days:
@@ -148,7 +229,7 @@ def cache_data(no_of_days: int = 365, force: bool = False) -> None:
             get_data_for_date(date_string, force)
         except HTTPError:
             pass
-        date -= datetime.timedelta(days=1)
+        date -= timedelta(days=1)
         date_string = date.strftime("%m-%d-%Y")
     print()
 
@@ -159,7 +240,7 @@ def validate_date(value, *_, **__):
             return latest_date.strftime("%m-%d-%Y")
         else:
             d = normalise_datetime(value)
-            if datetime.datetime.strptime(d, "%m-%d-%Y") > latest_date:
+            if datetime.strptime(d, "%m-%d-%Y") > latest_date:
                 logger.warning(f"Date for date '{d}' is not available. Using the latest data...")
                 return latest_date.strftime("%m-%d-%Y")
             return d
@@ -179,7 +260,7 @@ def main():
 def tally(date, countries):
     """Shows COVID-19 tally for countries."""
     click.echo(f"Selected date (MM-DD-YYYY) is {date}")
-    date_obj = datetime.datetime.strptime(date, "%m-%d-%Y")
+    date_obj = datetime.strptime(date, "%m-%d-%Y")
     data = []
     for ct in countries:
         try:
