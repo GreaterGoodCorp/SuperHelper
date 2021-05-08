38a2416ec7df7a66a2a8fc6e0bd9f2bc505be8fc
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-08 16:55:29 +0800

    Initial commit

---
+++ src/SuperHelper/Modules/PyInit/__main__.py
@@ -0,0 +1,40 @@
+import sys
+from pathlib import Path
+from subprocess import Popen
+
+import click
+
+from SuperHelper import AppDir
+from SuperHelper.Core.Utils import PathLike
+
+ModuleName = "PyInit"
+ModuleDir = AppDir / ModuleName
+
+
+def initialise_project_folder(name: str) -> PathLike:
+    return Path(name).absolute()
+
+
+def initialise_git(path: PathLike):
+    # Get CWD
+    cwd = Path(path).absolute()
+    cwd.mkdir(exist_ok=True, parents=True)
+    # Initialise git repo at 'path'
+    Popen(["git", "init", str(cwd)], stdout=None)
+    #
+    pass
+
+
+@click.group("py")
+def main():
+    """Python project tools."""
+    pass
+
+
+@main.command()
+@click.argument("name", required=True)
+def init(name):
+    """Initialises a new python project."""
+    path = initialise_project_folder(name)
+    initialise_git(path)
+    sys.exit(0)
---
+++ src/SuperHelper/Modules/PyInit/__meta__.py
@@ -0,0 +1,245 @@
+BaseCoverageConfig = """
+[run]
+branch = True
+source = src/
+
+[report]
+exclude_lines =
+    if self.debug:
+    if debug:
+    pragma: no cover
+    if __name__ == .__main__.:
+ignore_errors = True
+"""
+
+BaseGitIgnore = """
+# Byte-compiled / optimized / DLL files
+__pycache__/
+*.py[cod]
+*$py.class
+
+# C extensions
+*.so
+
+# Distribution / packaging
+.Python
+build/
+develop-eggs/
+dist/
+downloads/
+eggs/
+.eggs/
+lib/
+lib64/
+parts/
+sdist/
+var/
+wheels/
+pip-wheel-metadata/
+share/python-wheels/
+*.egg-info/
+.installed.cfg
+*.egg
+MANIFEST
+
+# PyInstaller
+#  Usually these files are written by a python script from a template
+#  before PyInstaller builds the exe, so as to inject date/other infos into it.
+*.manifest
+*.spec
+
+# Installer logs
+pip-log.txt
+pip-delete-this-directory.txt
+
+# Unit test / coverage reports
+htmlcov/
+.tox/
+.nox/
+.coverage
+.coverage.*
+.cache
+nosetests.xml
+coverage.xml
+*.cover
+*.py,cover
+.hypothesis/
+.pytest_cache/
+
+# Translations
+*.mo
+*.pot
+
+# Django stuff:
+*.log
+local_settings.py
+db.sqlite3
+db.sqlite3-journal
+
+# Flask stuff:
+instance/
+.webassets-cache
+
+# Scrapy stuff:
+.scrapy
+
+# Sphinx documentation
+docs/_build/
+
+# PyBuilder
+target/
+
+# Jupyter Notebook
+.ipynb_checkpoints
+
+# IPython
+profile_default/
+ipython_config.py
+
+# pyenv
+.python-version
+
+# pipenv
+#   According to pypa/pipenv#598, it is recommended to include Pipfile.lock in version control.
+#   However, in case of collaboration, if having platform-specific dependencies or dependencies
+#   having no cross-platform support, pipenv may install dependencies that don't work, or not
+#   install all needed dependencies.
+#Pipfile.lock
+
+# PEP 582; used by e.g. github.com/David-OConnor/pyflow
+__pypackages__/
+
+# Celery stuff
+celerybeat-schedule
+celerybeat.pid
+
+# SageMath parsed files
+*.sage.py
+
+# Environments
+.env
+.venv
+env/
+venv/
+ENV/
+env.bak/
+venv.bak/
+
+# Spyder project settings
+.spyderproject
+.spyproject
+
+# Rope project settings
+.ropeproject
+
+# mkdocs documentation
+/site
+
+# mypy
+.mypy_cache/
+.dmypy.json
+dmypy.json
+
+# Pyre type checker
+.pyre/
+
+# PyCharm
+.idea/
+"""
+
+BaseTravisConfig = """
+os: linux
+dist: xenial
+python: 3.9
+language: python
+before_install:
+  - python3 --version
+  - python3 -m pip install --upgrade pip
+install:
+  - pip3 install -r requirements.txt
+script: make test
+after_success: bash <(curl -s https://codecov.io/bash)
+"""
+
+BaseCodecovConfig = """
+codecov:
+  branch: main
+  require_ci_to_pass: true
+
+coverage:
+  precision: 2
+  round: nearest
+
+parsers:
+  gcov:
+    branch_detection:
+      conditional: yes
+      loop: yes
+      method: no
+      macro: no
+
+comment:
+  layout: "reach,diff,flags,files,footer"
+  behavior: default
+  require_changes: false
+"""
+
+BaseMakefile = """
+venv:
+	python3 -m venv .venv
+	./.venv/bin/pip3 install -r requirements.txt
+
+build:
+	python3 setup.py sdist bdist_wheel
+
+publish: build
+	twine check dist/* && twine upload dist/*
+
+dev-install:
+	pip3 install -e .
+
+test:
+	pytest --cov
+
+clean:
+	rm -rf build/ dist/ .coverage
+	find . -type d -name __pycache__ -exec rm -r {} \*
+"""
+
+BaseRequirements = """
+wheel
+twine
+pytest
+setuptools
+pytest-cov
+"""
+
+BaseSetup = """
+from setuptools import find_packages, setup
+
+with open("README.md") as fp:
+    long_desc = fp.read()
+
+setup(
+    name="SuperHelper",
+    version="0.0.0",
+    author="",
+    author_email="",
+    description="",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={
+    },
+    classifiers=[
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+    ],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
+    include_package_data=True,
+    python_requires=">=3.6",
+)
+"""
