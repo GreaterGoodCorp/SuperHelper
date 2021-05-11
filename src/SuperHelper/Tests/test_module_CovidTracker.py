79a44e951c5798496fbf24ee26c66c74d26a8226
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-11 18:59:57 +0800

    Remove all unoptimised tests

--- src/SuperHelper/Tests/test_module_CovidTracker.py
+++ src/SuperHelper/Tests/test_module_CovidTracker.py
@@ -44,16 +44,6 @@ class TestCovidTracker:
         return CACHE_DIR / f"extracted-{Path(get_valid_url).name.split('.')[0]}.json"
 
     @staticmethod
-    @pytest.fixture()
-    def get_raw_data(get_valid_url):
-        return get_source_file(get_valid_url)
-
-    @staticmethod
-    @pytest.fixture()
-    def get_parsed_data(get_raw_data):
-        return parse_source_data(get_raw_data)
-
-    @staticmethod
     def test_normalise_datetime(get_valid_date, get_invalid_date):
         assert normalise_datetime("31-12-2021")
         assert normalise_datetime(get_valid_date)
@@ -70,20 +60,3 @@ class TestCovidTracker:
             create_source_url("12-31-1999")
         with pytest.raises(ValueError):
             create_source_url("12-31-1999")
-
-    @staticmethod
-    def test_get_source_file(get_valid_url, get_invalid_url):
-        assert len(get_source_file(get_valid_url)) != 0
-        assert len(get_source_file(get_valid_url)) != 0
-        with pytest.raises(HTTPError):
-            get_source_file(get_invalid_url)
-
-    @staticmethod
-    def test_parse_source_file(get_raw_data):
-        assert len(parse_source_data(get_raw_data)) != 0
-
-    @staticmethod
-    def test_extract_source_data(get_parsed_data, get_cache_filename):
-        assert len(extract_source_data(get_parsed_data, get_cache_filename)) != 0
-        assert len(extract_source_data(get_parsed_data, get_cache_filename)) != 0
-        assert len(extract_source_data(get_parsed_data)) != 0
