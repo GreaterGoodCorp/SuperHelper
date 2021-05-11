9a8ae38bf895278e1c1f1d25e7ce75ffe4b0fd30
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-11 19:15:33 +0800

    Add docstrings

--- src/SuperHelper/Core/Essentials/__init__.py
+++ src/SuperHelper/Core/Essentials/__init__.py
@@ -1,4 +1,6 @@
 # This subpackage allows a one-off import statement of most frequently used functions for all modules
+import logging
+
 from SuperHelper import AppDir, DEBUG
 from SuperHelper.Core.Utils.type_hinting import PathLike
 from SuperHelper.Core.Utils.type_ensure import TypeCheck
@@ -9,7 +11,17 @@ from SuperHelper.Core.Utils.crypto_ops import Cryptographer
 from SuperHelper.Core.Config.config_class import pass_config
 
 
-def initialise_module(module_name: str):
+def initialise_module(module_name: str) -> (str, logging.Logger, PathLike):
+    """Initialises a SuperHelper module on startup.
+
+    Args:
+        module_name (str): Name of the module.
+
+    Returns:
+        A tuple of (name, logger, module_dir), where name is the fully qualified name of the module (should be assigned
+        to __name__), logger is a module-level logger, and module_dir is a Path instance of the path to module's own
+        directory to store data.
+    """
     mod_name = f"SuperHelper.Modules.{module_name}"
     mod_dir = AppDir / module_name
     mod_dir.mkdir(parents=True, exist_ok=True)
