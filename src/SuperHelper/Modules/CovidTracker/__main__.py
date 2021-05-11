451abcfe7fd4e5edff8d1f500784af08f784e3c4
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-11 19:06:33 +0800

    Code refactoring

--- src/SuperHelper/Modules/CovidTracker/__main__.py
+++ src/SuperHelper/Modules/CovidTracker/__main__.py
@@ -1,33 +1,28 @@
 import csv
-from datetime import datetime, timedelta
+import datetime as dt
 import io
 import json
 import sys
-from copy import deepcopy
-from urllib.error import HTTPError
-from urllib.request import urlopen
-from pathlib import Path
+import copy
+import urllib.error as url_err
+import urllib.request as url_req
+import pathlib
 import re
-import logging
 
 import numpy as np
 import matplotlib.pyplot as plt
-from dateutil.parser import parse
+import dateutil.parser
 import click
 
-import SuperHelper
-from SuperHelper.Core.Utils import PathLike
+from SuperHelper.Core.Essentials import *
 
-MODULE_NAME: str = "CovidTracker"
-MODULE_DIR = SuperHelper.AppDir / MODULE_NAME
-MODULE_DIR.mkdir(parents=True, exist_ok=True)
-CACHE_DIR = MODULE_DIR / "Cache"
-CACHE_DIR.mkdir(parents=True, exist_ok=True)
-__name__ = f"SuperHelper.Modules.{MODULE_NAME}"
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
+ModuleName: str = "CovidTracker"
+logger, ModuleDir = initialise_module(ModuleName)
+CacheDir = ModuleDir / "Cache"
+CacheDir.mkdir(parents=True, exist_ok=True)
+__name__ = f"SuperHelper.Modules.{ModuleName}"
 
-origin_date = datetime(day=2, month=12, year=2020)
+origin_date = dt.datetime(day=2, month=12, year=2020)
 
 __all__ = [
     "normalise_datetime",
@@ -39,7 +34,7 @@ __all__ = [
     "get_data_for_date",
     "cache_data",
     "main",
-    "CACHE_DIR",
+    "CacheDir",
 ]
 
 
@@ -55,7 +50,7 @@ def normalise_datetime(date_string: str) -> str:
         A date string in format MM-DD-YYYY.
     """
     try:
-        return parse(date_string, dayfirst=True).strftime("%m-%d-%Y")
+        return dateutil.parser.parse(date_string, dayfirst=True).strftime("%m-%d-%Y")
     except ValueError:
         raise ValueError("Invalid date format")
 
@@ -90,24 +85,24 @@ def get_source_file(url: str, force: bool = False) -> list[str]:
     Returns:
         A list of strings of the lines of the source file.
     """
-    filename = Path(url).name
-    source_file_location = MODULE_DIR / "Cache" / filename
+    filename = pathlib.Path(url).name
+    source_file_location = ModuleDir / "Cache" / filename
     if not force and source_file_location.is_file():
         with open(source_file_location) as fp:
             return fp.readlines()
-    raw_data = list(urlopen(url))
+    raw_data = list(url_req.urlopen(url))
     string_data = list(map(lambda s: str(s, "utf-8"), raw_data))
     with open(source_file_location, "w") as fp:
         fp.writelines(string_data)
     return string_data
 
 
-latest_date = datetime.today()
+latest_date = dt.datetime.today()
 while True:
     try:
         get_source_file(create_source_url(latest_date.strftime("%m-%d-%Y")))
-    except HTTPError:
-        latest_date -= timedelta(days=1)
+    except url_err.HTTPError:
+        latest_date -= dt.timedelta(days=1)
     else:
         break
 
@@ -143,7 +138,7 @@ def extract_source_data(parsed_data: list[str], cache_file: PathLike = None, for
     parsed_data.pop(0)
     starting_index = 7
     number_of_field = 4
-    if not force and cache_file is not None and Path(cache_file).is_file():
+    if not force and cache_file is not None and pathlib.Path(cache_file).is_file():
         with open(cache_file) as fp:
             return json.load(fp)
     data = dict()
@@ -177,11 +172,11 @@ def get_data_for_date(date_string: str, force: bool = False) -> dict[str, list]:
     url = create_source_url(date_string)
     source_file = get_source_file(url, force)
     parsed_source_file = parse_source_data(source_file)
-    cache_filename = CACHE_DIR / f"extracted-{Path(url).name.split('.')[0]}.json"
+    cache_filename = CacheDir / f"extracted-{pathlib.Path(url).name.split('.')[0]}.json"
     return extract_source_data(parsed_source_file, cache_filename, force)
 
 
-def get_country_data(country: str, start_date: datetime = None, end_date: datetime = None) -> dict[str, list]:
+def get_country_data(country: str, start_date: dt.datetime = None, end_date: dt.datetime = None) -> dict[str, list]:
     """Gets data for a country over a range of days.
 
     Args:
@@ -199,7 +194,7 @@ def get_country_data(country: str, start_date: datetime = None, end_date: dateti
         start_date = origin_date
     result = dict()
     for i in range((end_date - start_date).days + 1):
-        date_string = (start_date + timedelta(days=i)).strftime("%m-%d-%Y")
+        date_string = (start_date + dt.timedelta(days=i)).strftime("%m-%d-%Y")
         data = get_data_for_date(date_string)
         country_data = data.get(country, None)
         if country_data is not None:
@@ -221,7 +216,7 @@ def cache_data(no_of_days: int = 365, force: bool = False) -> None:
     Returns:
         None
     """
-    date = deepcopy(latest_date)
+    date = copy.deepcopy(latest_date)
     date_string = date.strftime("%m-%d-%Y")
     if (date - origin_date).days < no_of_days:
         no_of_days = (date - origin_date).days
@@ -229,15 +224,15 @@ def cache_data(no_of_days: int = 365, force: bool = False) -> None:
         click.echo(f"\rDownloading for {date_string}... ({i + 1}/{no_of_days})", nl=False)
         try:
             get_data_for_date(date_string, force)
-        except HTTPError:
+        except url_err.HTTPError:
             pass
-        date -= timedelta(days=1)
+        date -= dt.timedelta(days=1)
         date_string = date.strftime("%m-%d-%Y")
     click.echo()
 
 
-def get_new_changes(country: str, date: datetime) -> list:
-    yesterday, today = get_country_data(country, date - timedelta(days=1), date).values()
+def get_new_changes(country: str, date: dt.datetime) -> list:
+    yesterday, today = get_country_data(country, date - dt.timedelta(days=1), date).values()
     yesterday = np.array(yesterday)
     today = np.array(today)
     return today - yesterday
@@ -249,7 +244,7 @@ def validate_date(value, *_, **__):
             return latest_date.strftime("%m-%d-%Y")
         else:
             d = normalise_datetime(value)
-            if datetime.strptime(d, "%m-%d-%Y") > latest_date:
+            if dt.datetime.strptime(d, "%m-%d-%Y") > latest_date:
                 logger.warning(f"Date for date '{d}' is not available. Using the latest data...")
                 return latest_date.strftime("%m-%d-%Y")
             return d
@@ -283,7 +278,7 @@ def main():
 def tally(date, no_change, countries):
     """Shows COVID-19 tally for countries."""
     click.echo(f"Selected date (MM-DD-YYYY) is {date}")
-    date_obj = datetime.strptime(date, "%m-%d-%Y")
+    date_obj = dt.datetime.strptime(date, "%m-%d-%Y")
     data = []
     changes = []
     for ct in countries:
@@ -331,7 +326,8 @@ def plot(end, number_of_days, confirmed, death, recovered, active, country, scal
     click.echo(f"Selected number of days is {number_of_days}")
     x_data = np.linspace(-(number_of_days - 1), 0, number_of_days)
     try:
-        data = get_country_data(country, parse(end) - timedelta(days=number_of_days - 1), parse(end))
+        data = get_country_data(country, dateutil.parser.parse(end) - dt.timedelta(days=number_of_days - 1), dateutil
+                                .parser.parse(end))
     except ValueError:
         raise click.BadParameter(f"Country '{country}' is not found!")
     if confirmed:
@@ -347,7 +343,7 @@ def plot(end, number_of_days, confirmed, death, recovered, active, country, scal
     plt.xlabel("Number of days since the latest report")
     plt.ylabel("Number of cases")
     plt.legend(loc="best")
-    if not SuperHelper.DEBUG:
+    if not DEBUG:
         plt.show()
     sys.exit(0)
 
