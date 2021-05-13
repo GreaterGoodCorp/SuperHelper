74a77854a3c8f05cae4d04491cc3fed7adf4f8a3
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-13 15:08:56 +0800

    Change example of 'FocusEnabler' to 'PyInit'

--- src/SuperHelper/Tests/test_core_command.py
+++ src/SuperHelper/Tests/test_core_command.py
@@ -4,66 +4,66 @@ from SuperHelper.Tests import *
 class TestCore:
     @staticmethod
     def test_add_single():
-        assert run("add FocusEnabler").exit_code == 0
+        assert run("add PyInit").exit_code == 0
 
     @staticmethod
     def test_add_single_again():
-        assert run("add FocusEnabler").exit_code == 0
+        assert run("add PyInit").exit_code == 0
 
     @staticmethod
     def test_list_single_positive():
         result = run("list")
         assert result.exit_code == 0
-        assert "FocusEnabler" in result.output
+        assert "PyInit" in result.output
         assert "Stenographer" not in result.output
 
     @staticmethod
     def test_remove_single():
-        assert run("remove FocusEnabler").exit_code == 0
+        assert run("remove PyInit").exit_code == 0
 
     @staticmethod
     def test_list_single_negative():
         result = run("list")
         assert result.exit_code == 0
-        assert "FocusEnabler" not in result.output
+        assert "PyInit" not in result.output
         assert "Stenographer" not in result.output
 
     @staticmethod
     def test_add_multiple():
-        assert run("add FocusEnabler Stenographer").exit_code == 0
+        assert run("add PyInit Stenographer").exit_code == 0
 
     @staticmethod
     def test_list_multiple_positive():
         result = run("list")
         assert result.exit_code == 0
-        assert "FocusEnabler" in result.output
+        assert "PyInit" in result.output
         assert "Stenographer" in result.output
 
     @staticmethod
     def test_remove_multiple():
-        assert run("remove FocusEnabler Stenographer").exit_code == 0
+        assert run("remove PyInit Stenographer").exit_code == 0
 
     @staticmethod
     def test_list_multiple_negative():
         result = run("list")
         assert result.exit_code == 0
-        assert "FocusEnabler" not in result.output
+        assert "PyInit" not in result.output
         assert "Stenographer" not in result.output
 
     @staticmethod
     def test_list_all():
         result = run("list -a")
         assert result.exit_code == 0
-        assert "FocusEnabler" in result.output
+        assert "PyInit" in result.output
         assert "Stenographer" in result.output
 
     @staticmethod
     def test_add_same():
-        assert run("add FocusEnabler FocusEnabler").exit_code == 0
+        assert run("add PyInit PyInit").exit_code == 0
 
     @staticmethod
     def test_remove_same():
-        assert run("remove FocusEnabler FocusEnabler").exit_code == 0
+        assert run("remove PyInit PyInit").exit_code == 0
 
     @staticmethod
     def test_add_invalid():
