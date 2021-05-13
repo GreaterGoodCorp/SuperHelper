18ed3a76c95ce5b6d473198b8a3556842cfdd25f
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-13 14:58:53 +0800

    Initial commit

---
+++ src/SuperHelper/Modules/Grapher/__main__.py
@@ -0,0 +1,23 @@
+import click
+
+from SuperHelper.Core.Essentials import *
+
+ModuleName = "Grapher"
+__name__, logger, ModuleDir = initialise_module(ModuleName)
+
+
+def validate_equation():
+    # TODO: Do tokenization here!
+    pass
+
+
+@click.command("math")
+def main():
+    """Solves maths on commandline."""
+    pass
+
+
+@main.command("solve")
+@click.argument("equation", type=validate_equation, required=True)
+def solve(equation):
+    pass
