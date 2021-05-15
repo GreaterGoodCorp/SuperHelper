e3d7a27478029439240c7d816aed3f792aef7e28
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-16 00:51:53 +0800

    Write complete input validators

--- src/SuperHelper/Modules/Grapher/__main__.py
+++ src/SuperHelper/Modules/Grapher/__main__.py
@@ -11,13 +11,17 @@ CacheDir = ModuleDir / "Cache"
 def validate_equation(value, *_, **__):
     eqn_cache_dir = CacheDir / "EquationParser"
     eqn_cache_dir.mkdir(parents=True, exist_ok=True)
-    return EquationParser(outputdir=eqn_cache_dir).parse(value)
+    parser = EquationParser(outputdir=eqn_cache_dir)
+    parser.make_ast(value)
+    return parser.ast
 
 
 def validate_expression(value, *_, **__):
     exp_cache_dir = CacheDir / "ExpressionParser"
     exp_cache_dir.mkdir(parents=True, exist_ok=True)
-    return ExpressionParser(outputdir=exp_cache_dir).parse(value)
+    parser = ExpressionParser(outputdir=exp_cache_dir)
+    parser.make_ast(value)
+    return parser.generate_function()
 
 
 @click.group("math")
