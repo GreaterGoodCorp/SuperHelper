22f981f33a9e7a8cde62af24d96919af08e191c8
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-15 12:57:43 +0800

    Implement parsers

--- src/SuperHelper/Modules/Grapher/__main__.py
+++ src/SuperHelper/Modules/Grapher/__main__.py
@@ -1,14 +1,23 @@
 import click
 
 from SuperHelper.Core.Essentials import *
+from SuperHelper.Modules.Grapher import EquationParser, ExpressionParser
 
 ModuleName = "Grapher"
 __name__, logger, ModuleDir = initialise_module(ModuleName)
+CacheDir = ModuleDir / "Cache"
 
 
-def validate_equation():
-    # TODO: Do tokenization here!
-    pass
+def validate_equation(value, *_, **__):
+    eqn_cache_dir = CacheDir / "EquationParser"
+    eqn_cache_dir.mkdir(parents=True, exist_ok=True)
+    return EquationParser(outputdir=eqn_cache_dir).parse(value)
+
+
+def validate_expression(value, *_, **__):
+    exp_cache_dir = CacheDir / "ExpressionParser"
+    exp_cache_dir.mkdir(parents=True, exist_ok=True)
+    return ExpressionParser(outputdir=exp_cache_dir).parse(value)
 
 
 @click.group("math")
