b0b59da9405d0a96ac1f35c164291a77876f2017
Nguyen Thai Binh <binhnt.mdev@gmail.com>
2021-05-16 14:51:05 +0800

    Use fully qualified name

--- src/SuperHelper/Modules/Grapher/__init__.py
+++ src/SuperHelper/Modules/Grapher/__init__.py
@@ -1,5 +1,5 @@
-import lex
-import yacc
+import SuperHelper.Modules.Grapher.lex as lex
+import SuperHelper.Modules.Grapher.yacc as yacc
 from .lexer import UserInputLexer
 from .parser import EquationParser, ExpressionParser
 from .__main__ import main
